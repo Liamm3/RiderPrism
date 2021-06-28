d4544f88c70fb7f61396818964de2d27f992e2a9
Liam Wohlfart <liam.wohlfart0@gmail.com>
2021-06-28 16:05:51 +0200

    create sample prism project using intellij rider

---
+++ RiderPrism/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
+using RiderPrism.Views;
+
+namespace RiderPrism {
+   public partial class App : PrismApplication {
+      protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+      }
+
+      protected override Window CreateShell() => Container.Resolve<ShellWindow>();
+   }
+}
---
+++ RiderPrism/Views/ShellWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace RiderPrism.Views {
+    public partial class ShellWindow : Window {
+        public ShellWindow() {
+            InitializeComponent();
+        }
+    }
+}
