f48226b76195befcea5dfd7509706f1ec5570eb9
Liam Wohlfart <liam.wohlfart0@gmail.com>
2021-06-29 08:26:26 +0200

    add simple navigation

---
+++ RiderPrism/ViewModels/ShellWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace RiderPrism.ViewModels {
+    public class ShellWindowViewModel : BindableBase {
+       private readonly IRegionManager _regionManager;
+       public DelegateCommand<string> NavigateCommand { get; set; }
+
+       public ShellWindowViewModel(IRegionManager regionManager) {
+           _regionManager = regionManager;
+           NavigateCommand = new DelegateCommand<string>(Navigate);
+       }
+
+       private void Navigate(string uri) {
+           _regionManager.RequestNavigate("ContentRegion", uri);
+       }
+    }
+}
