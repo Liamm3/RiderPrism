971e1d03a76a52b8b2c4abfca8440b17ea1db53a
Liam Wohlfart <liam.wohlfart0@gmail.com>
2021-06-29 08:51:21 +0200

    add pageview counter to viewa

--- ModuleA/ViewModels/ViewAViewModel.cs
+++ ModuleA/ViewModels/ViewAViewModel.cs
@@ -1,7 +1,30 @@
 ﻿using Prism.Mvvm;
+using Prism.Regions;
 
 namespace ModuleA.ViewModels {
-    public class ViewAViewModel : BindableBase {
+    public class ViewAViewModel : BindableBase, INavigationAware {
+        private int _pageViews = 0;
+        private string _text = "ViewA";
 
+        public int PageViews {
+            get => _pageViews;
+            set => SetProperty(ref _pageViews, value);
+        }
+
+        public string Text {
+            get => _text;
+            set => SetProperty(ref _text, value);
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext) {
+            PageViews++;
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext) {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext) {
+        }
     }
 }
--- ModuleA/ViewModels/ViewBViewModel.cs
+++ ModuleA/ViewModels/ViewBViewModel.cs
@@ -2,6 +2,17 @@
 
 namespace ModuleA.ViewModels {
     public class ViewBViewModel : BindableBase {
+        private int _pageViews = 0;
+        private string _text = "ViewB";
 
+        public int PageViews {
+            get => _pageViews;
+            set => SetProperty(ref _pageViews, value);
+        }
+
+        public string Text {
+            get => _text;
+            set => SetProperty(ref _text, value);
+        }
     }
 }
